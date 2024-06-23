4300127fb47ce7253bf45d1ead2f5fa1c3934acc
MichaelSpiridonov <michael.sp579@gmail.com>
2024-06-23 23:21:43 +0300

    Fully Implement Book Details & Add On Sale Sign

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,13 +1,19 @@
-import { bookService } from "../services/book.service.js";
+import { bookService } from "../services/book.service.js"
 
 export function BookPreview({ book }) {
-    const {title, thumbnail } = book
-    const { amount: price, currencyCode } = book.listPrice
-    return (
-        <article className="book-preview">
-            <h1>Title: {title}</h1>
-            <h2>Price: {price}{bookService.getCurrencyCode(currencyCode)}</h2>
-            <img src={`./assets/img/${thumbnail}`} alt="" />
-        </article>
-    )
+  const { title, thumbnail } = book
+  const { amount: price, currencyCode, isOnSale } = book.listPrice
+  return (
+    <article className="book-preview">
+      <h1>Title: {title}</h1>
+      <h2>
+        Price: {price}
+        {bookService.getCurrencyCode(currencyCode)}
+      </h2>
+      <div className="image-container">
+        <img src={`./assets/img/${book.thumbnail}`} alt="" />
+        {(isOnSale) ? <div className="sale-sign">On Sale!</div> : ''}
+      </div>
+    </article>
+  )
 }
--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -3,22 +3,61 @@ import { bookService } from "../services/book.service.js"
 const { useEffect, useState } = React
 
 export function BookDetails({ bookId, onBack }) {
+  const [book, setBook] = useState(null)
 
-    const [book, setBook] = useState(null)
+  useEffect(() => {
+    bookService.get(bookId).then((book) => setBook(book))
+  }, [])
 
-    useEffect(() => {
-        bookService.get(bookId)
-            .then(book => setBook(book))
-    }, [])
-    
-    if (!book) return <div className="loader"></div>
-    return (
-        <section className="book-details">
-            <img src={`./assets/img/${book.thumbnail}`} alt="" />
-            <h1>Book Title: {book.title}</h1>
-            <h1>Book Price: {`${book.listPrice.amount} ${bookService.getCurrencyCode(book.listPrice.currencyCode)}`}</h1>
-            <p>Book Description: {book.description}</p>
-            <button onClick={onBack}>Back</button>
-        </section>
-    )
+  function getPageCategory() {
+    if (book.pageCount > 500) return "Serious Reading"
+    else if (book.pageCount > 200) return "Decent Reading"
+    else return "Light Reading"
+  }
+
+  function getCondition(publishedDate) {
+    const currentYear = getCurrentYear()
+    if (currentYear - publishedDate > 10) return "Vintage"
+    else if (currentYear - publishedDate < 1) return "New"
+    else return "Not New /Neither Old"
+  }
+
+  function getCurrentYear() {
+    return new Date().getFullYear()
+  }
+
+  function colorPrice() {
+    if (book.listPrice.amount > 150) return "red"
+    else if (book.listPrice.amount < 20) return "green"
+  }
+
+  if (!book) return <div className="loader"></div>
+  return (
+    <section className="book-details">
+      <div className="image-container">
+        <img src={`./assets/img/${book.thumbnail}`} alt="" />
+        {book.listPrice.isOnSale ? (<div className="sale-sign">On Sale!</div>) : ''}
+      </div>
+      <h1>Book Title: {book.title}</h1>
+      <h1>
+        Book Price:
+        <span className={colorPrice()}>{`${
+          book.listPrice.amount
+        } ${bookService.getCurrencyCode(book.listPrice.currencyCode)}`}</span>
+      </h1>
+      <h2>is On Sale: {book.listPrice.isOnSale ? "Yes" : "No"}</h2>
+      <ul>
+        <li>Author/s: {book.authors.map((author) => `${author}, `)}</li>
+        <li>Page Count: {book.pageCount}</li>
+        <li>Page Category: {getPageCategory()}</li>
+        <li>Published Date: {book.publishedDate}</li>
+        <li>Condition: {getCondition(book.publishedDate)}</li>
+        <li>Categories: {book.categories}</li>
+        <li>Language: {book.language}</li>
+      </ul>
+      <p>Book Description: {book.description}</p>
+
+      <button onClick={onBack}>Back</button>
+    </section>
+  )
 }
